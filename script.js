2b8854c0f17db398310a1f642fcd10db552924ae
FDevFull <134306423+Belfores@users.noreply.github.com>
2026-03-18 16:30:30 +0100

    Create script.js file for product page functionality

---
+++ script.js
@@ -0,0 +1,12 @@
+// JavaScript functionality for the product page
+
+// Example: Add to Cart Functionality
+function addToCart(productId) {
+    console.log('Product added to cart:', productId);
+    // Your cart logic here
+}
+
+// Call this function when the add to cart button is clicked
+// document.getElementById('addToCartButton').onclick = function() {
+//     addToCart(productId);
+// };
